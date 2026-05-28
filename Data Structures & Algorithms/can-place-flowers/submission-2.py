7708d8f4448b01f6765cf5c63e60776670f4315b
MadDev280901 <zm778sm5md@privaterelay.appleid.com>
2026-05-27 22:01:42 -0400

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for spot in range(len(flowerbed)):
+            adjacent = []
+            if flowerbed[spot] == 0:
+                if spot+1 < len(flowerbed):
+                    adjacent.append(spot+1)
+                if spot-1 < len(flowerbed):
+                    adjacent.append(spot-1)
+                
+                canPlant = True
+                for neighbor in adjacent:
+                    if flowerbed[neighbor] == 1:
+                        canPlant = False
+                
+                if canPlant:
+                    flowerbed[spot] = 1
+                    n-=1
+                
+        
+        print(flowerbed)
+        return True if n <= 0 else False
+
+
+        
