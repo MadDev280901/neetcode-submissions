32a65573dfea029a322c9c1485421e76452a224a
MadDev280901 <zm778sm5md@privaterelay.appleid.com>
2026-05-28 10:10:09 -0400

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ctr = 0 
+        expected = sorted(heights)
+        for i in range(0, len(heights)):
+            if heights[i] != expected[i]:
+                ctr+=1
+        
+        return ctr
+        
