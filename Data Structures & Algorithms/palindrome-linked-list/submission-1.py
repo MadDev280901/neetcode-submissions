52032ef797af26f8eb92c5a743144333d9d10ee5
MadDev280901 <zm778sm5md@privaterelay.appleid.com>
2026-05-28 18:19:28 -0400

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isPalindrome(self, head):
+        if not head or not head.next:
+            return True
+
+        # find middle
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        prev = None
+        cur = slow
+
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        # compare
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+
+            left = left.next
+            right = right.next
+
+        return True
