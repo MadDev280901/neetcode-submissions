37ef07a48e64e41cbb73f7154659aa5960ea71e1
MadDev280901 <zm778sm5md@privaterelay.appleid.com>
2026-08-28 23:41:22 -0400

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res, trip = [], []
+
+        def helper(k, start, target):
+            if k != 2:
+                for i in range(start, len(nums)-k+1):
+                    if i > start and nums[i] == nums[i-1]:
+                        continue
+                    trip.append(nums[i])
+                    helper(k-1, i+1, target-nums[i])
+                    trip.pop()
+                return 
+            l, r = start, len(nums)-1
+            while l < r: 
+                if nums[l] + nums[r] < target:
+                    l+=1
+                elif nums[l] + nums[r] > target:
+                    r-=1
+                else:
+                    res.append(trip + [nums[l], nums[r]])
+                    l+=1
+
+                    while l < r and nums[l] == nums[l-1]:
+                        l+=1
+        
+        helper(3, 0, 0)
+        return res
+
+            
+
+
+
+        
